eff263bbaf0531ced79d46cca05cbcdd420438f7
acemark1 <jeyachandranajinath@gmail.com>
2021-11-16 15:19:19 +0000

    Fixed API request Error

--- server.js
+++ server.js
@@ -8,9 +8,24 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors()); 
 
 app.get("/api", (req, res)=>{ 
-    console.log("Called");
-    res.json({users: ["user1", "user2"]}); 
-})
+    res.json({"users": ["user1", "user2"]})
 
+})
+ 
 app.listen(9000, ()=>{console.log("Server started on port 9000")})
 
+// res.json({
+//     id: "1", 
+//     projectName:"Nodlehs",
+//     projectDetail:"We need a process that will track the inventory and status of components in the chassis.\
+//               It should accept IPC requests from the various cards, maintain an internal database of what's currently available,\
+//               respond to IPC queries from other components and have a CLI interface so that we can query its contents. We will be\
+//               adding capabilities above and beyond this in the future.",
+//     projectDue:"Tomorrow",
+//     projectPriority:"1",
+//     projectChekced:"false",
+//     projectLoggedInIcon:"./images/sheldon.jpg",
+//     projectLoggedInUsername:"sheldcooper93",
+//     ProjectTag:""  // Make this array
+    
+// }); 
